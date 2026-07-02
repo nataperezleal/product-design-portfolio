5bd5d3be4c0e2c71dde571a8ed6e4a38fe9e3658
Natalia Perez <nataperezleal@gmail.com>
2026-07-02 16:34:19 -0500

    nuevos cambios

--- js/main.js
+++ js/main.js
@@ -107,6 +107,6 @@ initCarousel('meli', 2);  // 2 images
 initCarousel('kuadrado', 5);
 initCarousel('greentech', 4);
 initCarousel('herbal', 4);
-initCarousel('pantene', 3);
+initCarousel('pantene', 2);
 initCarousel('hs', 3);
 initCarousel('social', 3);
